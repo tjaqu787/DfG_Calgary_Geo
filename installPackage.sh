31725c100b633f3cc30218244f46c51809c9789d
Tom Hauser <tom.hauser@gmail.com>
2022-06-16 21:30:18 -0400

    chore: adding rudimentary installation scripts

---
+++ installPackage.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+pip install wheel
+pip install pip
+pip install sodapy
+pip install numpy
+pip install pandas
+pip install shapely
+pip install gdal
+pip install fiona
+pip install pyproj
+pip install six
+pip install rtree
+pip install geopandas
