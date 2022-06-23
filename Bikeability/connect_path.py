a2de271c8d1aa991e19a67f3900b2a2e073b47b9
Helder <heldercro@gmail.com>
2022-06-22 18:33:43 -0600

    upd: plotting all the paths together

--- Bikeability/connect_path.py
+++ Bikeability/connect_path.py
@@ -1,24 +1,27 @@
 import osmnx as ox
 import folium
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from data_apis import City_Of_Calgary_ARCGIS_Rest_API as cgy
 
 map = folium.Map(location=[51.0719, -114.048], zoom_start=12)
 
+lanes = {}
+lanes['red'] = cgy.get_protected_bike_lanes()
+lanes['green'] = cgy.get_bike_routes()
+lanes['blue'] = cgy.get_painted_bike_lanes()
+lanes['magenta'] = cgy.get_regional_pathways()
 
-bike_lane = cgy.get_painted_bike_lanes()
-print(bike_lane.shape)
+graph = pd.DataFrame()
+for k,v in lanes.items():
+    graph = pd.concat([graph, v], axis=0)
+    print(v.shape)
+    folium.Choropleth(v, line_weight=3, line_color=k).add_to(map)
 
-reg_path = cgy.get_regional_pathways()
-print(reg_path.shape)
 
-graph = reg_path
-graph = pd.concat([bike_lane, reg_path], axis=0)
 print(graph.shape)
 
-folium.Choropleth(reg_path, line_weight=3, line_color='red').add_to(map)
-folium.Choropleth(bike_lane, line_weight=3, line_color='blue').add_to(map)
 
 # show the map in a jupyter notebook
 map
