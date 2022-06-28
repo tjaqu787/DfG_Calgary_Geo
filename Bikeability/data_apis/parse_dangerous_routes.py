5db97fffdeefdce7702014ffe1e0d57e9aa1cf67
tjaqu787 <tyrelljaques@gmail.com>
2022-06-27 18:26:44 -0600

    Takes the city of calgary traffic flow and bike route network and returns safe and dangerous routes.

--- Bikeability/data_apis/parse_dangerous_routes.py
+++ Bikeability/data_apis/parse_dangerous_routes.py
@@ -1,16 +1,21 @@
 import geopandas as gpd
+import folium
+from shapely.ops import snap,split
+
+def split_line_by_point(line, tolerance: float=1.0e-12):
+    return split(snap(line, line.centroid, tolerance), line.centroid)
 
 
-def remove_dangerous_routes(route_df,network_df):
+def remove_dangerous_routes(route_df, network_df):
     """
-
     :param route_df: CoC bike route geo dataframe
     :param network_df: CoC traffic flow volume geo dataframe
     :return: safe routes ,dangerous routes
     """
-    dangerous = gpd.sjoin(route_df,network_df)
+    route_df['geometry'] = route_df.geometry.apply(split_line_by_point)
+    dangerous = gpd.sjoin(route_df, network_df)
     safe = route_df[~route_df['OBJECTID'].isin(dangerous['OBJECTID'])]
-    return safe,dangerous
+    return safe, dangerous
 
 
 if __name__ == '__main__':
@@ -23,5 +28,5 @@ if __name__ == '__main__':
     traffic_data = traffic_data.explode(index_parts=False)
     traffic_data.crs = route_data.crs
 
-    x,y = remove_dangerous_routes(route_data,traffic_data)
+    x, y = remove_dangerous_routes(route_data, traffic_data)
     print()
