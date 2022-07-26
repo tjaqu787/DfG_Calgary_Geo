5c217f15631efd1acce7fe748d14a8a2a94c4295
tjaqu787 <60409230+tjaqu787@users.noreply.github.com>
2022-07-26 17:17:45 -0600

    Add files via upload

--- Bikeability/data_apis/connectors.py
+++ Bikeability/data_apis/connectors.py
@@ -2,21 +2,26 @@ import pandas
 from sodapy import Socrata
 import os.path
 
-source_hostname="data.calgary.ca"
-pathways_endpoint = "6eun-p5zf"
-file_name = "pathways.pkl"
 
-if __name__=='__main__':
+def get_connector_data():
+
+    source_hostname="data.calgary.ca"
+    pathways_endpoint = "6eun-p5zf"
+    file_name = "pathways.pkl"
     if not os.path.exists(file_name):
         print(f'{file_name} does not exist. Downloading new data...')
         client = Socrata('data.calgary.ca', None)
         results = client.get(pathways_endpoint)
         dataframe = pandas.DataFrame.from_records(results)
         print(f'Data downloaded. Saving to {file_name}...')
-        dataframe.to_pickle(file_name)
+        dataframe.to_json(file_name)
     else:
         print(f'{file_name} exists. Loading data from disk...')
-        dataframe = pandas.read_pickle(file_name)
+        dataframe = pandas.read_json(file_name)
 
-    print(dataframe)
 
+    return dataframe
+
+
+if __name__ == '__main__':
+    get_connector_data()
